f27af7a7500a8d3aeb5347d3c01bf3e89b9d3bb6
RaquelDiazdj <dh24001@esfe.agape.edu.sv>
2024-04-02 22:27:58 -0600

    se agrego al comentario

---
+++ CDF1SchoolShopEN/VentasEN.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDF1SchoolShopEN
+{
+    internal class VentasEN
+    {
+        public int id { get; set; }
+        public string Producto { get; set; }
+
+    }
+}
