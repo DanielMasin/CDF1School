6f7519843e40fe1689171a75f9448b678165df22
SARA ABIGAHÍL DÍAZ HERNÁNDEZ . <2683334@estudiantes.mined.edu.sv>
2024-04-03 14:35:27 -0600

    Formulario

--- CDF1SchoolShopUI/ScchoolShopInventarioWF.cs
+++ CDF1SchoolShopUI/ScchoolShopInventarioWF.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CDF1SchoolShopUI
 {
-    public partial class SchoolShopInventarioWF : Form
+    public partial class ScchoolShopInventarioWF : Form
     {
-        public SchoolShopInventarioWF()
+        public ScchoolShopInventarioWF()
         {
             InitializeComponent();
         }
