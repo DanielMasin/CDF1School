8efe04fe186429f2419e6d5d89aa8b9e26389d55
SARA ABIGAHÍL DÍAZ HERNÁNDEZ . <2683334@estudiantes.mined.edu.sv>
2024-04-03 14:18:07 -0600

    Agregue Formulario

--- CDF1SchoolShopBL/InventarioBL.cs
+++ CDF1SchoolShopBL/InventarioBL.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CDF1SchoolShopEN;
+using CDF1SchoolShopDAL;
 
 namespace CDF1SchoolShopBL
 {
     internal class InventarioBL
     {
+        private InventarioDAL InventarioDAL = new InventarioDAL();
 
-
+        public void GuardarInventarioEN(InventarioEN pInventarioEN)
+        {
+            InventarioDAL.GuardarInventarioEN(pInventarioEN);
+        }
+        public List<InventarioEN> ObtenerTodosInventarioEN()
+        {
+            return InventarioDAL.ObtenerTodosInventarioEN();
+        }
     }
 }
--- CDF1SchoolShopDAL/InventarioDAL.cs
+++ CDF1SchoolShopDAL/InventarioDAL.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CDF1SchoolShopEN;
 
 namespace CDF1SchoolShopDAL
 {
-    internal class InventarioDAL
+    public  class InventarioDAL
     {
+        private static List<InventarioEN> ListInventarioEN = new List<InventarioEN>();
 
+        public void GuardarInventarioEN(InventarioEN pInventarioEN)
+        {
+            pInventarioEN.id = ListInventarioEN.Select(ListIventarioEN => ListInventarioEN.id).DefaultIfEmpty(0).Max() + 1;
+            ListInventarioEN.Add(pInventarioEN);
+        }
+        public List<InventarioEN> ObtenerTodosInventarioEN()
+        {
+            return ListInventarioEN;
+        }
     }
 }
