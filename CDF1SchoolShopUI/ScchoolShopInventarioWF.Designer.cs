26fab271217e56ea236226c9930c0e783577edcc
SARA ABIGAHÍL DÍAZ HERNÁNDEZ . <2683334@estudiantes.mined.edu.sv>
2024-04-03 23:17:04 -0600

    cambios en formulario

--- CDF1SchoolShopUI/ScchoolShopInventarioWF.Designer.cs
+++ CDF1SchoolShopUI/ScchoolShopInventarioWF.Designer.cs
@@ -28,18 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            contextMenuStrip1 = new ContextMenuStrip(components);
+            textBuscar = new TextBox();
+            lista = new ListBox();
+            btnBuscar = new Button();
+            button1 = new Button();
             SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            contextMenuStrip1.Name = "contextMenuStrip1";
+            contextMenuStrip1.Size = new Size(61, 4);
+            // 
+            // textBuscar
+            // 
+            textBuscar.Location = new Point(26, 38);
+            textBuscar.Name = "textBuscar";
+            textBuscar.Size = new Size(323, 23);
+            textBuscar.TabIndex = 2;
+            // 
+            // lista
+            // 
+            lista.FormattingEnabled = true;
+            lista.ItemHeight = 15;
+            lista.Location = new Point(26, 87);
+            lista.Name = "lista";
+            lista.Size = new Size(442, 244);
+            lista.TabIndex = 4;
+            // 
+            // btnBuscar
+            // 
+            btnBuscar.Location = new Point(364, 38);
+            btnBuscar.Name = "btnBuscar";
+            btnBuscar.Size = new Size(104, 23);
+            btnBuscar.TabIndex = 5;
+            btnBuscar.Text = "Buscar Codigo";
+            btnBuscar.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(517, 365);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 6;
+            button1.Text = "Salir";
+            button1.UseVisualStyleBackColor = true;
             // 
             // ScchoolShopInventarioWF
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(697, 417);
+            Controls.Add(button1);
+            Controls.Add(btnBuscar);
+            Controls.Add(lista);
+            Controls.Add(textBuscar);
             Name = "ScchoolShopInventarioWF";
-            Text = "ScchoolShopInventarioWF";
+            Text = "SchoolShopInventarioWF";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private ContextMenuStrip contextMenuStrip1;
+        private TextBox textBuscar;
+        private ListBox lista;
+        private Button btnBuscar;
+        private Button button1;
     }
 }
