97e432d886fb77b16c55a1c6458c01dbe742114d
SARA ABIGAHÍL DÍAZ HERNÁNDEZ . <2683334@estudiantes.mined.edu.sv>
2024-04-07 17:52:21 -0600

    Codigo

--- CDF1SchoolShopEN/InventarioEN.cs
+++ CDF1SchoolShopEN/InventarioEN.cs
@@ -10,12 +10,11 @@ namespace CDF1SchoolShopEN
     //Arreglos para almacenar la informacion de productos de la mini tienda
     public class InventarioEN
     {
-        public int id;
-        public int[] Registro;
-        public string[] Codigo;
-        public string[] Preciounitario;
-        public int[] Marca;
-        public string[] Producto;
-        public string[] Inversioneninventario;
+        public int id { get; set; }
+        public string Codigo { get; set; }
+        public string Preciounitario { get; set; }
+        public int Marca { get; set; } 
+        public string Producto { get; set; }
+        public string Inversioneninventario { get; set; }
     }
 }
